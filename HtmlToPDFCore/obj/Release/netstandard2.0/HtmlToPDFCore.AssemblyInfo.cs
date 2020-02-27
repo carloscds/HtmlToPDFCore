523de7308d95c63f54aca7d28998491824b3a247
Carlos dos Santos <cds@cds-software.com.br>
2020-02-27 12:32:19 -0300

    Nuget config

--- HtmlToPDFCore/obj/Release/netstandard2.0/HtmlToPDFCore.AssemblyInfo.cs
+++ HtmlToPDFCore/obj/Release/netstandard2.0/HtmlToPDFCore.AssemblyInfo.cs
@@ -10,8 +10,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HtmlToPDFCore")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Carlos dos Santos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("HTMLToPDF convert HTML to PDF File.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HtmlToPDFCore")]
